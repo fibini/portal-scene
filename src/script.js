48d28adff550e70108de11bccba743448f4f47f5
Webdev <fabienswift@gmail.com>
2023-01-18 19:37:12 -0400

    Add custom shader for fireflies

--- src/script.js
+++ src/script.js
@@ -4,6 +4,8 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import firefliesVertexShader from './shaders/fireflies/vertex.glsl'
+import firefliesFragmentShader from './shaders/fireflies/fragment.glsl'
 
 /**
  * Base
@@ -96,6 +98,38 @@ gltfLoader.load(
 )
 
 /**
+ * Fireflies
+ */
+// Geometry
+const firefliesGeometry = new THREE.BufferGeometry()
+const firefliesCount = 50
+const positionArray = new Float32Array(firefliesCount * 3)
+
+for(let i = 0; i < firefliesCount; i++)
+{
+    positionArray[i * 3 + 0] = (Math.random() - 0.5) * 4
+    positionArray[i * 3 + 1] = Math.random() * 1
+    positionArray[i * 3 + 2] = (Math.random() - 0.5) * 4
+}
+firefliesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3))
+
+// Material
+const firefliesMaterial = new THREE.ShaderMaterial({
+    uniforms:
+    {
+        uPixelRatio: {value: Math.min(window.devicePixelRatio, 2)},
+        uSize: {value: 100}
+    },
+    vertexShader: firefliesVertexShader,
+    fragmentShader: firefliesFragmentShader
+})
+
+gui.add(firefliesMaterial.uniforms.uSize,'value').min(0).max(500).step(1).name('firefliesSize')
+
+// Points
+const fireflies = new THREE.Points(firefliesGeometry, firefliesMaterial)
+scene.add(fireflies)
+/**
  * Sizes
  */
 const sizes = {
@@ -116,6 +150,9 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    // Update fireflies
+    firefliesMaterial.uniforms.uPixelRatio.value = Math.min(window.devicePixelRatio, 2)
 })
 
 /**
---
+++ src/shaders/fireflies/fragment.glsl
@@ -0,0 +1,4 @@
+void main()
+{
+    gl_FragColor = vec4(gl_PointCoord, 0.0, 1.0);
+}
