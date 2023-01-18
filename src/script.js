76e683e63786e8a5ca637718e62b2f7929d3c341
Webdev <fabienswift@gmail.com>
2023-01-18 18:35:14 -0400

    Add background color

--- src/script.js
+++ src/script.js
@@ -9,6 +9,7 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
  * Base
  */
 // Debug
+const debugObject = {}
 const gui = new dat.GUI({
     width: 400
 })
@@ -141,6 +142,15 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.outputEncoding = THREE.sRGBEncoding
+
+debugObject.clearColor = '#0b1123'
+renderer.setClearColor(debugObject.clearColor)
+gui
+    .addColor(debugObject, 'clearColor')
+    .onChange(() =>
+    {
+        renderer.setClearColor(debugObject.clearColor)
+    })
 
 /**
  * Animate
