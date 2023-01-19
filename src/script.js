d30562b71a8e3ae2b82efd835a7578f412e622df
Webdev <fabienswift@gmail.com>
2023-01-18 21:07:51 -0400

    Add animation and randomness to fireflies

--- src/script.js
+++ src/script.js
@@ -104,24 +104,32 @@ gltfLoader.load(
 const firefliesGeometry = new THREE.BufferGeometry()
 const firefliesCount = 50
 const positionArray = new Float32Array(firefliesCount * 3)
+const scaleArray = new Float32Array(firefliesCount)
 
 for(let i = 0; i < firefliesCount; i++)
 {
     positionArray[i * 3 + 0] = (Math.random() - 0.5) * 4
     positionArray[i * 3 + 1] = Math.random() * 1
     positionArray[i * 3 + 2] = (Math.random() - 0.5) * 4
+
+    scaleArray[i] = Math.random()
 }
 firefliesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3))
+firefliesGeometry.setAttribute('aScale', new THREE.BufferAttribute(scaleArray, 1))
 
 // Material
 const firefliesMaterial = new THREE.ShaderMaterial({
     uniforms:
     {
-        uPixelRatio: {value: Math.min(window.devicePixelRatio, 2)},
-        uSize: {value: 100}
+        uTime: { value: 0 },
+        uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+        uSize: { value: 100 }
     },
     vertexShader: firefliesVertexShader,
-    fragmentShader: firefliesFragmentShader
+    fragmentShader: firefliesFragmentShader,
+    transparent: true,
+    blending: THREE.AdditiveBlending,
+    depthWrite: false
 })
 
 gui.add(firefliesMaterial.uniforms.uSize,'value').min(0).max(500).step(1).name('firefliesSize')
@@ -197,6 +205,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update materials
+    firefliesMaterial.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
